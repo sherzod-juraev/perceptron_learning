30ae0fdaec0fe6503d6b5fe9e67aa1eb3385e238
JuraevSherzod <juraevsherzod9943@gmail.com>
2025-11-18 16:24:12 +0500

    main file created and settings file updated in core package

---
+++ core/settings.py
@@ -0,0 +1,14 @@
+from pydantic_settings import BaseSettings
+
+
+class Setting(BaseSettings):
+    database_url: str
+    algorithm: str
+    secret_key: str
+    access_token_minutes: int
+    refresh_token_days: int
+
+    class Config:
+
+        env_file = '.env'
+        env_file_encoding = 'utf-8'
