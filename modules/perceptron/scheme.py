15b12dda955e49708a4066e27ed08b41826a54be
JuraevSherzod <juraevsherzod9943@gmail.com>
2025-11-21 00:41:38 +0500

    scheme file updated again in perceptron package of modules package

--- modules/perceptron/scheme.py
+++ modules/perceptron/scheme.py
@@ -1,6 +1,12 @@
 from pydantic import BaseModel
 
 
+class PerceptronOut(BaseModel):
+
+    fit: str = True
+    weights: list[float]
+
+
 class PerceptronIn(BaseModel):
     model_config = {
         'extra': 'forbid'
