55cbd4fbd25af3d2d93b25d699ddb9e58c47881e
JuraevSherzod <juraevsherzod9943@gmail.com>
2025-11-21 00:06:54 +0500

    model file updated in perceptron package

---
+++ perceptron/model.py
@@ -0,0 +1,50 @@
+from numpy import array, ndarray, float64, dot, unique
+from .preprocessing import preprocessing
+from fastapi import HTTPException, status
+from numpy.random import uniform
+
+
+class Perceptron:
+    def __init__(self, learning_rate: float, n_iter: int, /):
+
+        self.eta = learning_rate
+        self.n_iter = n_iter
+        self.weights = None
+        self.bias = round(uniform(-.01, .01).item(), 4)
+        self.classes = None
+
+    def fit(self, X: ndarray, Y: ndarray, /) -> bool:
+
+        preprocessing(X, Y)
+        self.classes = unique(Y)
+        self.initialize_weight(X.shape[1])
+        length = X.shape[0]
+        for i in range(self.n_iter):
+            result = True
+            for j in range(length):
+                z = self.predict(X[j])
+                if Y[j] != z:
+                    self.update_features(X[j], Y[j]-z)
+                    result = False
+            if result:
+                return True
+        return False
+
+    def initialize_weight(self, array_size: int, /):
+
+        self.weights = uniform(-.01, .01, size=array_size)
+
+    def net_input(self, X: ndarray, /) -> float64:
+
+        z = dot(X, self.weights) + self.bias
+        return z
+
+    def predict(self, X: ndarray, /) -> int:
+
+        z = self.classes[0] if self.net_input(X) >= 0 else self.classes[1]
+        return int(z)
+
+    def update_features(self, X: ndarray, error: int, /):
+
+        self.weights += self.eta * error * X
+        self.bias += self.eta * error
--- perceptron/preprocessing.py
+++ perceptron/preprocessing.py
@@ -1,11 +1,11 @@
-from numpy import array
+from numpy import array, ndarray, integer
 from numpy import unique, issubdtype, isnan, number
 from fastapi import HTTPException, status
 
 
-def preprocessing(X: array, Y: array, /):
+def preprocessing(X: ndarray, Y: ndarray, /):
 
-    if X.ndim() != 2:
+    if X.ndim != 2:
         raise HTTPException(
             status_code=status.HTTP_422_UNPROCESSABLE_CONTENT,
             detail='A 2D matrix must be inserted into X'
@@ -15,13 +15,23 @@ def preprocessing(X: array, Y: array, /):
             status_code=status.HTTP_422_UNPROCESSABLE_CONTENT,
             detail='NaN values found!'
         )
+    if not issubdtype(X.dtype, number):
+        raise HTTPException(
+            status_code=status.HTTP_422_UNPROCESSABLE_CONTENT,
+            detail='All features must be numeric!'
+        )
     if len(unique(Y)) != 2:
         raise HTTPException(
             status_code=status.HTTP_422_UNPROCESSABLE_CONTENT,
             detail='Target must have exactly 2 classes!'
         )
-    if not issubdtype(X.dtype, number):
+    if not issubdtype(Y.dtype, integer):
         raise HTTPException(
             status_code=status.HTTP_422_UNPROCESSABLE_CONTENT,
-            detail='All features must be numeric!'
+            detail='classes must be integer'
+        )
+    if X.shape[0] != Y.shape[0]:
+        raise HTTPException(
+            status_code=status.HTTP_422_UNPROCESSABLE_CONTENT,
+            detail='The lengths X and Y are not compatible.'
         )
